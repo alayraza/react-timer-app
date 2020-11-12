5597a06633c9eb4c6c30ff6ec7fe4b7fc4d7163a
alayraza <50444174+alayraza@users.noreply.github.com>
2020-11-13 01:49:35 +0500

    update

--- src/components/random.tsx
+++ src/components/random.tsx
@@ -5,7 +5,7 @@ import { timerBtnsPropsType } from "../types/timer_TYPE";
 const Random: React.FC<timerBtnsPropsType> = () => {
   return (
     <div className="container">
-      <h1>hamzah</h1>
+      <h1>Alay</h1>
     </div>
   );
 };
